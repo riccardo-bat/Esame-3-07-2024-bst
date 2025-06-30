bb3d0c824739862287954c578a11d580a02731b8
RB <350798@studenti.unimore.it>
2025-06-30 21:59:20 +0200

    Punto 2b completato

--- compito.c
+++ compito.c
@@ -1,11 +1,16 @@
 #include "hotel.h"
 #include "bst.h"
+#include <stdbool.h>
 
 #define MAX_CARATTERI_BUFFER 100
 
 //prototipi 
+int int_input(char* msg, int minimum_value, int max_value, size_t maxLengthinput);
+void string_input(char* msg, char* dest, size_t maxLengthInput);
 void create_bst(FILE* fp, bst* bst_hotels);
 void stampa(bst t);
+void ricerca(bst, int, bool);
+int media(bst bst_hotels, int threshold, char* place);
 
 int main(){
     //PUNTO 1   
@@ -23,6 +28,25 @@ int main(){
     stampa(bst_hotels);
 
 
+    //punto 2a
+    printf("\n\n--------------------------------------------------");
+    int threshold_stelle = int_input("\nInserire il numero di stelle come limite: ", 1, 5, 1);
+    printf("\n1) valuta gli hotel con le stelle > di %d. \n0) altrimenti", threshold_stelle);
+    bool maggiori = int_input("\nValore: ", 0, 1, 1); 
+
+    //1 = true, 0 = false
+    ricerca(bst_hotels, threshold_stelle, maggiori);
+
+    //punto 2b
+    printf("\n\n--------------------------------------------------");
+    char place[MAX_PLACE_LEN]; 
+    string_input("\nInserire il luogo per il calcolo della media: ", place, MAX_PLACE_LEN);
+    threshold_stelle = int_input("\nInserire il numero di stelle come limite: ", 1, 5, 1);
+
+    printf("\n\nIl calcolo della media, stabiliti i parametri [soglia = %d, luogo = '%s'], risulta %d",
+         threshold_stelle, place, media(bst_hotels, threshold_stelle, place));
+
+
 
     //chiusura dei file aperti
     if(fclose(file_hotel) == EOF){
@@ -32,6 +56,49 @@ int main(){
 
     printf("\n\n");
     return 0;
+}
+
+int int_input(char* msg, int minimum_value, int max_value, size_t maxLengthinput){
+    printf("%s", msg); 
+    char buffer[maxLengthinput+1]; //includo il '\0'
+
+    while(fgets(buffer, (maxLengthinput+1), stdin) == NULL){
+        printf("\n\t-Input non valido, reinserire: ");
+    }
+
+    //a questo punto devo pulire il buffer
+    int c; 
+    while((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    char* endpoint = NULL;
+    int to_return = (int) strtol(buffer, &endpoint, 10);
+
+    //se nessun carattere è stato convertito
+    if(endpoint == buffer || to_return < minimum_value || to_return > max_value){
+        printf("\n\nERRORE NELL'ACQUISIZIONE DELL'INPUT AL PUNTO 2A");
+        return int_input(msg, minimum_value, max_value, maxLengthinput);
+    }
+
+    return to_return;
+}
+
+void string_input(char* msg, char* dest, size_t maxLengthInput){
+    printf("%s", msg);
+    char buffer[maxLengthInput+1]; 
+
+    while(fgets(buffer, maxLengthInput+1, stdin) == NULL){
+        printf("\n\t- Reinserire: ");
+    }
+
+    //svuoto il buffer
+    int c;
+    while((c=getchar()) != '\n' && c != EOF)
+        ;
+
+    //rimpiazzo il primo '\n' con il terminatore
+    buffer[strcspn(buffer, "\n")] = '\0'; //al posto del "\n" inserisco il terminatore
+    strcpy(dest, buffer);
 }
 
 
@@ -109,4 +176,56 @@ void stampa(bst t){
     print_key(t->key);
 
     stampa(t->right);
+}
+
+/**
+ * @brief Tramite una visit DFS valuto maggiore per stampare gli hotel con un numero di stelle
+ * > threshold se maggiori = 1 = true, i minori altrimenti
+ * 
+ * @param bst_hotels 
+ * @param threshold 
+ * @param maggiori 
+ */
+void ricerca(bst bst_hotels, int threshold, bool maggiori){
+    //caso base --> albero vuoto
+    if(bst_hotels == NULL) return;
+
+    //sfrutto la stampa INORDER per stampare secondo l'ordine decrescente delle chiavi
+    ricerca(bst_hotels->left, threshold, maggiori);
+    
+    //stampo il nodo
+    //se maggiori = true stampo solo gli hotel con un numero di stelle > soglia
+    if((maggiori && bst_hotels->key > threshold) || (!maggiori && bst_hotels->key <= threshold)){
+        print_tipo_inf(bst_hotels->inf);
+        printf(" --> ");
+        print_key(bst_hotels->key);
+    } //else --> se maggiori = true ma la chiave è minore (e viceversa) non stampo nulla
+
+    //se la chiave del nodo attuale è già minore della soglia, non vado a vedere i nodi minore del nodo stesso
+    ricerca(bst_hotels->right, threshold, maggiori);
+}
+
+/**
+ * @brief Calcolo il numero di hotel nel luogo specificato con un #stelle > soglia. Per contarli 
+ * in ordine decrescente uso una DFS in order
+ * 
+ * @param bst_hotels 
+ * @param threshold 
+ * @param place 
+ * @return int 
+ */
+int media(bst bst_hotels, int threshold, char* place){
+    if(bst_hotels == NULL) return 0; 
+    int tot = 0;
+
+    //visita in order
+    tot += media(bst_hotels->left, threshold, place);
+
+    //analizzo il nodo 
+    if(strcmp(bst_hotels->inf.luogo, place) == 0 && bst_hotels->key > threshold)
+        tot++; 
+
+    //se il nodo attule ha una chiave già minore della soglia, è inutile andare a destra verso nodi con chiavi più piccole
+    tot += media(bst_hotels->right, threshold, place);
+    return tot;
 }
